8acc804b592322885287c33015acba8df1b87af2
Batyrq14 <utegenov1403@gmail.com>
2024-02-23 20:04:15 +0600

    No2 is done

--- No2 Homework/app/src/main/java/com/batyrcode/no2homework/MainActivity.kt
+++ No2 Homework/app/src/main/java/com/batyrcode/no2homework/MainActivity.kt
@@ -2,10 +2,18 @@ package com.batyrcode.no2homework
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.batyrcode.no2homework.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonChange.setOnClickListener {
+            binding.imageView2.setImageResource(R.drawable.iamrich_02)
+        }
     }
 }
